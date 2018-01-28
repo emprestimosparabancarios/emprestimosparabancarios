fc8383bcf7b9f57c698a1e1740dd38ff89b2f215
Emprestimo Para Bancarios <emprestimosparabancarios@gmail.com>
2018-01-28 14:50:04 -0200

    arquivos corrigidos

---
+++ formulario.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1463801-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+<title>Empr&eacute;stimo com Cheque. Parcelas que cabem no seu bolso. Em at&eacute; 60x.</title>
+<meta name="description" content="Empréstimo com cheque. Entenda como funciona e faça um orçamento sem compromisso."/>
+<meta name="keywords" content="emprestimo com cheque, emprestimo com cheque em sp"/>
+<meta name="expires" content="never"/>
+<meta name="revisit-after" content="01 days"/>
+<meta name="service" content="emprestimo consignado inss, emprestimo consignado, aposentado"/>
+<meta name="category" content=" emprestimo consignado inss, emprestimo consignado, aposentado"/>
+<meta name="language" content="pt-br, português, portuguese"/>
+<meta name="geo. region" content="br, brasil, brazil"/>
+<meta name="copyright" content="2010-2010, emprestimo consignado inss, emprestimo consignado, aposentado "/>
+<meta name="author" content="lourenço, ayslan"/>
+<meta name="email" content="emprestimoosasco@yahoo.com.br"/>
+<meta name="rating" content="general"/>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+
+<script language="JavaScript"> 
+function abrir(URL) {
+ 
+  var width = 592;
+  var height = 1050;
+ 
+  var left = 99;
+  var top = 99;
+ 
+  window.open(URL,'janela', 'width='+width+', height='+height+', top='+top+', left='+left+', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
+ 
+}
+</script>
+
+</head>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+<link rel="stylesheet" type="text/css" href="css/global.css"/>
+
+<body>
+<div class="baseBg">
+<div class="base">
+
+    <div class="header">
+      <div class="headerLeft">&nbsp;
+        </div>
+      <div class="headerCenter">
+            <div class="hcLeft">
+                <ul>
+                    <li class="liPri"><a rel="nofollow" title="Empréstimos para Bancários" href="index.html"><img src="imagens/emprestimos-para-bancarios.png" alt="Empréstimos para Bancários" title="Empréstimos para Bancários" /></a></li>
+                    <li class="liSeg"><img src="imagens/emprestimos-para-bancarios-11-930054005.png" alt="Empréstimos para Bancários (11) 93005-4005" title="Empréstimos para Bancários (11) 93005-4005" /></li>
+                </ul>
+            </div>
+            <div class="hcRight">
+        <img src="imagens/emprestimos-para-bancarios-cheque.png" alt="Empréstimos para Bancários" title="Empréstimos para Bancários" />
+            </div>
+      </div>
+      <div class="headerRight">
+        </div>
+    </div>
+
+    <div class="content">
+      <div class="contentLeft">
+          <div class="contentLeftForm2">
+              <img src="imagens/solicite-orcamento-gratis.png" alt="Solicite Orcamento Grátis" title="Solicite Orcamento Grátis" />
+            </div>
+          <div class="contentLeftForm">
+
+<?
+
+// Coloque a mensagem que irá ser enviada para seu e-mail abaixo:
+if (preg_match('/tempsite.ws$|locaweb.com.br$|hospedagemdesites.ws$|websiteseguro.com$/', $_SERVER[HTTP_HOST])) {
+          $email_from='contato@emprestimosparabancarios.com.br'; // Substitua essa linha pelo seu e-mail@seudominio
+          } else {
+          $email_from = 'contato@emprestimosparabancarios.com.br' . $_SERVER[HTTP_HOST]; 
+                    ##### "webmaster@" . $_SERVER[HTTP_HOST]; // sem www no endereço do browser
+          //    Na linha acima estamos forçando que o remetente seja 'webmaster@seudominio',
+          // você pode alterar para que o remetente seja, por exemplo, 'contato@seudominio'.
+          }
+           
+          /* Verifica qual é o sistema operacional do servidor para ajustar o cabeçalho de forma correta. Não alterar */
+          if(PHP_OS == "Linux") $quebra_linha = "\n"; //Se for Linux
+          elseif(PHP_OS == "WINNT") $quebra_linha = "\r\n"; // Se for Windows
+          else die("Este script nao esta preparado para funcionar com o sistema operacional de seu servidor");
+           
+          // Passando os dados obtidos pelo formulário para as variáveis abaixo
+          $nomeremetente     = "Fulano de Tal";
+          $emailremetente    = trim("contato@emprestimosparabancarios.com.br");
+          $emaildestinatario = trim("rogerio@liguecred.com.br");
+#          $comcopia          = trim("rogerio@liguecred.com.br");
+		  $comcopia          = trim("clemerson.azevedo@gmail.com");
+#          $comcopiaoculta    = trim("rogerio@liguecred.com.br");
+          $assunto           = "Empréstimo Para Bancários, Cadastro Completo";
+#                   $mensagem          = "Mensagem enviada em ".date("d/m/Y").", os dados seguem abaixo:n";
+                    $mensagem          = "Mensagem enviada com os dados abaixo:n";
+
+           
+          /* Montando a mensagem a ser enviada no corpo do e-mail. */
+          $mensagemHTML = '<P>Empréstimo Para Bancários, Contato</P>
+            <b>  Nome:</b> '.$_POST['nome'].' 
+            <b> <br>E-mail:</b> '.$_POST['email'].' 
+            <b> <br>Tel:</b> '.$_POST['dddfixo'].'-'.$_POST['fixo'].'
+            <b> <br>Cel:</b> '.$_POST['dddcel'].'-'.$_POST['cel'].'
+            <b> <br>Valor:</b> '.$_POST['valor'].'
+            <b> <br>Possui nome sujo?</b> '.$_POST['pnomesujo'].'
+            <b> <br>Trabalha em qual banco?</b> '.$_POST['trabalha'].'
+            <b> <br>Salário:</b> '.$_POST['salario'].'
+            <b> <br>Observações:</b> '.$_POST['observacoes'].'
+            <p><b><i>'.$mensagem.'</i></b></p>
+            <hr>';
+           
+           
+          /* Montando o cabeçalho da mensagem */
+          $headers = "MIME-Version: 1.1".$quebra_linha;
+          $headers .= "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
+          // Perceba que a linha acima contém "text/html", sem essa linha, a mensagem não chegará formatada.
+          $headers .= "From: ".$email_from.$quebra_linha;
+          $headers .= "Return-Path: " . $email_from . $quebra_linha;
+          // Esses dois "if's" abaixo são porque o Postfix obriga que se um cabeçalho for especificado, deverá haver um valor.
+          // Se não houver um valor, o item não deverá ser especificado.
+          if(strlen($comcopia) > 0) $headers .= "Cc: ".$comcopia.$quebra_linha;
+          if(strlen($comcopiaoculta) > 0) $headers .= "Bcc: ".$comcopiaoculta.$quebra_linha;
+          $headers .= "Reply-To: ".$emailremetente.$quebra_linha;
+          // Note que o e-mail do remetente será usado no campo Reply-To (Responder Para)
+           
+          /* Enviando a mensagem */
+            if(mail($emaildestinatario, $assunto, $mensagemHTML, $headers )){
+              echo '<div class="formBack" style="color:ff0000">Obrigado pela mensagem. <br> Entraremos em contato o mais breve!</div>';
+            }
+            else{
+              echo '<div class="formBack" style="color:ff0000">Erro ao enviar!</div>';
+            }  
+            
+echo '<a href="javascript:window.history.go(-1)">Voltar</a>';
+?>
+            <h6 class="pPrivacidade"><a href="javascript:abrir('politica-de-privacidade.html');">Política de Privacidade</a></h6>
+            </div>
+            <div class="contentLeftBase">
+              <img src="img/bgFormBase.png" alt="" />
+            </div>
+            <div class="contentLeftLogo">
+                <img src="imagens/emprestimos-para-bancarios-losango-panamericano-bv.png" alt="Empréstimos para Bancários - Losando - Panamericano - BV" title="Empréstimos para Bancários - Losando - Panamericano - BV" />
+            </div>
+        </div>
+        <div class="contentRightAll">
+          <div class="menu">
+                <ul>
+                    <li><a rel="nofollow" title="Empréstimos para Bancários" href="index.html">Home</a></li>
+                    <li><a rel="nofollow" title="Quem Somos" href="quem-somos.html">Quem Somos</a></li>
+                    <li><a rel="nofollow" title="Empréstimos para Bancários" href="emprestimos-para-bancarios.html">Empréstimos para Bancários</a></li>
+                    <li><a rel="nofollow" title="Cadastro Completo" href="cadastro-completo.html">Cadastro Completo</a></li>
+                </ul>
+            </div>
+
+          <div class="crTop"><img src="img/ligue_emprestimo_siape_top.png" /></div>
+            <div class="contentRight">
+              <img src="img/contato_page.jpg" />
+            </div>
+            <div class="crBase"><img src="img/ligue_emprestimo_siape_base.png" /></div>
+        </div>
+    </div>
+    <div class="footer">
+        <h6>Sartorato Informações Cadastrais LTDA - ME</h6>
+        <h6>Av. Pres. Médice, n° 69 sala 02, Jd Aliança, Osasco - SP, CEP 06268-000</h6>
+        <h6>CNPJ 06.301.745/0001-77, desde 27/05/2004</h6>
+        <h6>Todos os Direitos Reservados</h6>
+    </div>
+
+
+</div>
+</div>
+</body>
+</html>
---
+++ formularioCadastro.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1463801-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+
+<title>Solicite seu Empr&eacute;stimo com Cheque. Ligue ou preencha o cadastro.</title>
+<meta name="description" content="Solicite já o seu empréstimo com cheque. Temos as melhores taxas para o estado de São Paulo."/>
+<meta name="keywords" content="emprestimo com cheque, emprestimo com cheque em sp"/>
+<meta name="expires" content="never"/>
+<meta name="revisit-after" content="01 days"/>
+<meta name="service" content="emprestimo consignado inss, emprestimo consignado, aposentado"/>
+<meta name="category" content=" emprestimo consignado inss, emprestimo consignado, aposentado"/>
+<meta name="language" content="pt-br, português, portuguese"/>
+<meta name="geo. region" content="br, brasil, brazil"/>
+<meta name="copyright" content="2010-2010, emprestimo consignado inss, emprestimo consignado, aposentado "/>
+<meta name="author" content="lourenço, ayslan"/>
+<meta name="email" content="emprestimoosasco@yahoo.com.br"/>
+<meta name="rating" content="general"/>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+
+<script language="JavaScript"> 
+function abrir(URL) {
+ 
+  var width = 592;
+  var height = 1050;
+ 
+  var left = 99;
+  var top = 99;
+ 
+  window.open(URL,'janela', 'width='+width+', height='+height+', top='+top+', left='+left+', scrollbars=yes, status=no, toolbar=no, location=no, directories=no, menubar=no, resizable=no, fullscreen=no');
+ 
+}
+</script>
+
+</head>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+<link rel="stylesheet" type="text/css" href="css/global.css"/>
+
+<body>
+<div class="baseBg">
+<div class="base">
+
+    <div class="header">
+      <div class="headerLeft">&nbsp;
+        </div>
+      <div class="headerCenter">
+            <div class="hcLeft">
+                <ul>
+                    <li class="liPri"><a rel="nofollow" title="Empréstimos para Bancários" href="index.html"><img src="imagens/emprestimos-para-bancarios.png" alt="Empréstimos para Bancários" title="Empréstimos para Bancários" /></a></li>
+                    <li class="liSeg"><img src="imagens/emprestimos-para-bancarios-11-930054005.png" alt="Empréstimos para Bancários (11) 93005-4005" title="Empréstimos para Bancários (11) 93005-4005" /></li>
+                </ul>
+            </div>
+            <div class="hcRight">
+        <img src="imagens/emprestimos-para-bancarios-cheque.png" alt="Empréstimos para Bancários" title="Empréstimos para Bancários" />
+            </div>
+      </div>
+      <div class="headerRight">
+        </div>
+    </div>
+
+    <div class="content">
+      <div class="contentLeft">
+          <div class="contentLeftForm2">
+            <img src="img/seloGratis.png" alt="" />
+            </div>
+          <div class="contentLeftForm">
+
+<?
+
+// Coloque a mensagem que irá ser enviada para seu e-mail abaixo:
+if (preg_match('/tempsite.ws$|emprestimosparabanca.hospedagemdesites.ws$|hospedagemdesites.ws$|websiteseguro.com$/', $_SERVER[HTTP_HOST])) {
+          $email_from='contato@emprestimosparabanca.hospedagemdesites.ws'; // Substitua essa linha pelo seu e-mail@seudominio
+          } else {
+          $email_from = 'contato@emprestimosparabanca.hospedagemdesites.ws' . $_SERVER[HTTP_HOST]; 
+          ##### "webmaster@" . $_SERVER[HTTP_HOST]; // sem www no endereço do browser
+          //    Na linha acima estamos forçando que o remetente seja 'webmaster@seudominio',
+          // você pode alterar para que o remetente seja, por exemplo, 'contato@seudominio'.
+          }
+           
+          /* Verifica qual é o sistema operacional do servidor para ajustar o cabeçalho de forma correta. Não alterar */
+          if(PHP_OS == "Linux") $quebra_linha = "\n"; //Se for Linux
+          elseif(PHP_OS == "WINNT") $quebra_linha = "\r\n"; // Se for Windows
+          else die("Este script nao esta preparado para funcionar com o sistema operacional de seu servidor");
+           
+          // Passando os dados obtidos pelo formulário para as variáveis abaixo
+          $nomeremetente     = "Fulano de Tal";
+          $emailremetente    = trim("contato@emprestimosparabanca.hospedagemdesites.ws");
+          $emaildestinatario = trim("rogeriosartorato@hotmail.com");
+          $comcopia          = trim("rogerio@liguecred.com.br");
+          $comcopia          = trim("clemerson.azevedo@gmail.com");
+#          $comcopiaoculta    = trim("rogerio@liguecred.com.br");
+          $assunto           = "Empréstimo Para Bancários, Cadastro Completo";
+          $mensagem          = "Mensagem enviada com os dados abaixo:n";
+
+           
+          /* Montando a mensagem a ser enviada no corpo do e-mail. */
+          $mensagemHTML = '<P>Empréstimo Para Bancários, Cadastro Completo</P>
+            <b> <br>Nome Completo:</b> '.$_POST['nome'].' 
+            <b> <br>Email:</b> '.$_POST['email'].' 
+            <b> <br>CPF:</b> '.$_POST['cpf'].' 
+            <b> <br>Identidade:</b> '.$_POST['identidade'].' 
+            <b> <br>Naturalidade:</b> '.$_POST['naturalidade'].' 
+            <b> <br>UF:</b> '.$_POST['uf'].' 
+            <b> <br>Data de Nascimento:</b> '.$_POST['datanasc'].' 
+            <b> <br>Sexo:</b> '.$_POST['sexo'].' 
+            <b> <br>Estado Civil:</b> '.$_POST['estadocivil'].' 
+            <b> <br>Qtde. Dependentes:</b> '.$_POST['qtd_dep'].' 
+            <b> <br>Nome do Pai:</b> '.$_POST['nomepai'].' 
+            <b> <br>Nome da Mãe:</b> '.$_POST['nomemae'].' 
+            <b> <br>Endereço:</b> '.$_POST['endereco'].' 
+            <b> <br>Nº:</b> '.$_POST['numero'].' 
+            <b> <br>Complemento:</b> '.$_POST['complemento'].' 
+            <b> <br>Bairro:</b> '.$_POST['bairro'].' 
+            <b> <br>Cidade:</b> '.$_POST['cidade'].' 
+            <b> <br>Estado:</b> '.$_POST['uf2'].' 
+            <b> <br>CEP:</b> '.$_POST['cep'].' 
+            <b> <br>Tipo de Residência:</b> '.$_POST['tpresidencia'].' 
+            <b> <br>Tempo de Residência:</b> '.$_POST['tempores'].' 
+            <b> <br>DDD:</b> '.$_POST['dddres'].' 
+            <b> <br>Telefone:</b> '.$_POST['foneres'].' 
+            <b> <br>DDD:</b> '.$_POST['dddcel'].' 
+            <b> <br>Telefone Celular:</b> '.$_POST['fonecel'].' 
+            <b> <br>Empresa:</b> '.$_POST['empresa'].' 
+            <b> <br>Tempo de empresa:</b> '.$_POST['tempoempresa'].' 
+            <b> <br>Endereço:</b> '.$_POST['enderecoemp'].' 
+            <b> <br>Nº:</b> '.$_POST['numeroemp'].' 
+            <b> <br>Complemento:</b> '.$_POST['complementoemp'].' 
+            <b> <br>Bairro:</b> '.$_POST['bairroemp'].' 
+            <b> <br>Cidade:</b> '.$_POST['cidadeemp'].' 
+            <b> <br>Estado:</b> '.$_POST['ufemp'].' 
+            <b> <br>CEP:</b> '.$_POST['cepemp'].' 
+            <b> <br>DDD:</b> '.$_POST['dddemp'].' 
+            <b> <br>Telefone:</b> '.$_POST['foneemp'].' 
+            <b> <br>Ramal:</b> '.$_POST['ramalemp'].' 
+            <b> <br>Cargo:</b> '.$_POST['cargo'].' 
+            <b> <br>Salário / Rendimento:</b> '.$_POST['salario'].' 
+            <b> <br>2ª atividade:</b> '.$_POST['seg_atividade'].' 
+            <b> <br>Tempo:</b> '.$_POST['tempo'].' 
+            <b> <br>DDD:</b> '.$_POST['dddemp2'].' 
+            <b> <br>Telefone:</b> '.$_POST['foneemp2'].' 
+            <b> <br>Ramal:</b> '.$_POST['ramalemp2'].' 
+            <b> <br>Salário / Rendimento:</b> '.$_POST['salario2'].' 
+            <b> <br>Nome:</b> '.$_POST['nomeref1'].' 
+            <b> <br>Parentesco:</b> '.$_POST['select'].' 
+            <b> <br>DDD:</b> '.$_POST['dddref1'].' 
+            <b> <br>Telefone:</b> '.$_POST['foneref2'].' 
+            <b> <br>Nome:</b> '.$_POST['nomeref2'].' 
+            <b> <br>Parentesco:</b> '.$_POST['select2'].' 
+            <b> <br>DDD:</b> '.$_POST['dddref2'].' 
+            <b> <br>Telefone:</b> '.$_POST['foneref1'].' 
+            <b> <br>Banco:</b> '.$_POST['banco'].' 
+            <b> <br>Agência:</b> '.$_POST['agencia'].' 
+            <b> <br>Conta:</b> '.$_POST['conta'].' 
+            <b> <br>Tipo:</b> '.$_POST['tipo'].' 
+            <b> <br>Desde:</b> '.$_POST['desde'].' 
+            <b> <br>Observações:</b> '.$_POST['obs'].' 
+            <p><b><i>'.$mensagem.'</i></b></p>
+            <hr>';
+           
+           
+          /* Montando o cabeçalho da mensagem */
+          $headers = "MIME-Version: 1.1".$quebra_linha;
+          $headers .= "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
+          // Perceba que a linha acima contém "text/html", sem essa linha, a mensagem não chegará formatada.
+          $headers .= "From: ".$email_from.$quebra_linha;
+          $headers .= "Return-Path: " . $email_from . $quebra_linha;
+          // Esses dois "if's" abaixo são porque o Postfix obriga que se um cabeçalho for especificado, deverá haver um valor.
+          // Se não houver um valor, o item não deverá ser especificado.
+          if(strlen($comcopia) > 0) $headers .= "Cc: ".$comcopia.$quebra_linha;
+          if(strlen($comcopiaoculta) > 0) $headers .= "Bcc: ".$comcopiaoculta.$quebra_linha;
+          $headers .= "Reply-To: ".$emailremetente.$quebra_linha;
+          // Note que o e-mail do remetente será usado no campo Reply-To (Responder Para)
+           
+          /* Enviando a mensagem */
+
+            if(mail($emaildestinatario, $assunto, $mensagemHTML, $headers ,"-r".$email_from)){
+              echo '<div class="formBack" style="color:ff0000">Obrigado pela mensagem. <br> Entraremos em contato o mais breve!</div>';
+            }
+            else{
+              echo '<div class="formBack" style="color:ff0000">Erro ao enviar!</div>';
+            }  
+            
+echo '<a href="javascript:window.history.go(-1)">Voltar</a>';
+?>
+            </div>
+            <div class="contentLeftBase">
+            <img src="img/bgFormBase.png" alt="" />
+            </div>
+            <div class="contentLeftLogo">
+            <img src="imagens/emprestimos-para-bancarios-losango-panamericano-bv.png" alt="Empréstimos para Bancários - Losando - Panamericano - BV" title="Empréstimos para Bancários - Losando - Panamericano - BV" />
+            </div>
+        </div>
+        <div class="contentRightAll">
+          <div class="menu">
+                <ul>
+                    <li><a rel="nofollow" title="Empréstimos para Bancários" href="index.html">Home</a></li>
+                    <li><a rel="nofollow" title="Quem Somos" href="quem-somos.html">Quem Somos</a></li>
+                    <li><a rel="nofollow" title="Empréstimos para Bancários" href="emprestimos-para-bancarios.html">Empréstimos para Bancários</a></li>
+                    <li><a rel="nofollow" title="Cadastro Completo" href="cadastro-completo.html">Cadastro Completo</a></li>
+                </ul>
+            </div>
+
+          <div class="crTop"><img src="img/ligue_emprestimo_siape_top.png" />
+            </div>
+            <div class="contentRight">
+            <img src="img/contato_page.jpg" />
+            </div>
+            <div class="crBase"><img src="img/ligue_emprestimo_siape_base.png" /></div>
+    <h6 class="pPrivacidade"><a href="javascript:abrir('politica-de-privacidade.html');">Política de Privacidade</a></h6>
+        </div>
+    </div>
+    <div class="footer">
+        <h6>Sartorato Informações Cadastrais LTDA - ME</h6>
+        <h6>Av. Pres. Médice, n° 69 sala 02, Jd Aliança, Osasco - SP, CEP 06268-000</h6>
+        <h6>CNPJ 06.301.745/0001-77, desde 27/05/2004</h6>
+        <h6>Todos os Direitos Reservados</h6>
+    </div>
+
+
+</div>
+</div>
+</body>
+</html>
